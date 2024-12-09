d534f4baad7a717ba5020c4476dbf5e27f50de5e
Vladimir Dinic <vladimir88net@gmail.com>
2024-12-09 01:09:50 +0100

    Update App.tsx

--- box-content-preview-demo/src/App.tsx
+++ box-content-preview-demo/src/App.tsx
@@ -1,8 +1,34 @@
 import "./App.css";
 import ContentPreview from "box-ui-elements/es/elements/content-preview";
+import IntlProvider from "react-intl";
 
 function App() {
-  return <ContentPreview>bravo</ContentPreview>;
+  return (
+    <IntlProvider locale="en">
+      <ContentPreview
+        contentSidebarProps={{
+          detailsSidebarProps: {
+            hasAccessStats: true,
+            hasClassification: true,
+            hasNotices: true,
+            hasProperties: true,
+            hasRetentionPolicy: true,
+            hasVersions: true,
+          },
+          features: FEATURES,
+          hasActivityFeed: true,
+          hasMetadata: true,
+          hasSkills: true,
+          hasVersions: true,
+        }}
+        hasHeader={true}
+        features={FEATURES}
+        fileId={FILE_ID}
+        token={TOKEN}
+        {...PROPS}
+      />
+    </IntlProvider>
+  );
 }
 
 export default App;
